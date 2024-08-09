5ec62e2d4da7cc934dcbdc23799e7db1144eb9f0
rajyadav <rajyadav751811@gmail.com>
2024-08-09 07:39:28 +0530

    Second Mini Project Using Thymeleaf as User Interface

--- 03.Mini-Projects/02_Mini_Project/src/main/java/in/raj/controller/EnquiryController.java
+++ 03.Mini-Projects/02_Mini_Project/src/main/java/in/raj/controller/EnquiryController.java
@@ -1,4 +1,24 @@
 package in.raj.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class EnquiryController {
+    @GetMapping("/dashboard")
+    public String dashBoardPage() {
+        return "dashboard";
+    }
+
+    @GetMapping("/enquiry")
+    public String enquiryPage() {
+        return "addEnquiry";
+    }
+
+    @GetMapping("/enquiries")
+    public String viewEnquiryPage() {
+        return "viewEnquiry";
+    }
+
 }
--- 03.Mini-Projects/02_Mini_Project/src/main/java/in/raj/controller/UserController.java
+++ 03.Mini-Projects/02_Mini_Project/src/main/java/in/raj/controller/UserController.java
@@ -1,4 +1,29 @@
 package in.raj.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+
+    @GetMapping("/signup")
+    public String signUpPage() {
+        return "signup";
+    }
+
+    @GetMapping("/unlock")
+    public String unlockPage() {
+        return "unlock";
+    }
+
+    @GetMapping("/forgot")
+    public String forgotPwdPage() {
+        return "forgotPwd";
+    }
 }
