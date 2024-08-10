423ada2cd8977ddebfde60f42ef93c011b987a1a
rajyadav <rajyadav751811@gmail.com>
2024-08-10 09:22:17 +0530

    Second Mini Project Using Thymeleaf as User Interface

--- 03.Mini-Projects/02_Mini_Project/src/main/java/in/raj/binding/DashboardResponse.java
+++ 03.Mini-Projects/02_Mini_Project/src/main/java/in/raj/binding/DashboardResponse.java
@@ -1,4 +1,7 @@
 package in.raj.binding;
 
 public class DashboardResponse {
+    private Integer totalEnquiriesCnt;
+    private Integer enrolledCnt;
+    private Integer lostCnt;
 }
--- 03.Mini-Projects/02_Mini_Project/src/main/java/in/raj/binding/EnquiryForm.java
+++ 03.Mini-Projects/02_Mini_Project/src/main/java/in/raj/binding/EnquiryForm.java
@@ -1,4 +1,10 @@
 package in.raj.binding;
 
 public class EnquiryForm {
+    private String studentName;
+    private Long studentPhno;
+    private String classMode;
+    private String courseName;
+    private String enqStatus;
+
 }
