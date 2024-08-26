38251a67da56a2fc6b2404a63e9d424b5a5e9dad
Raj Yadav <rajyadav751811@gmail.com>
2024-08-26 08:32:45 +0530

    Spring Constant Literal implemented

---
+++ 05_REST_API/src/main/java/in/raj/constant/AppConstant.java
@@ -0,0 +1,6 @@
+package in.raj.constant;
+
+public class AppConstant {
+    public static final String  WELCOME_MSG = "welcomeMsg";
+    public static final String  GREET_MSG = "greetMsg";
+}
--- 05_REST_API/src/main/java/in/raj/restcontroller/DemoController.java
+++ 05_REST_API/src/main/java/in/raj/restcontroller/DemoController.java
@@ -1,5 +1,6 @@
 package in.raj.restcontroller;
 
+import in.raj.constant.AppConstant;
 import in.raj.propertiesclass.AppProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @Autowired
     private AppProperties appProperties;
+
     @GetMapping("/p1")
     public String getWelcomeMsg(){
-        return appProperties.getMessages().get("welcomeMsg");
+        return appProperties.getMessages().get(AppConstant.WELCOME_MSG);
     }
     @GetMapping("/p2")
     public String getGreetMsg(){
-        return appProperties.getMessages().get("greetMsg");
+        return appProperties.getMessages().get(AppConstant.GREET_MSG);
     }
 }
