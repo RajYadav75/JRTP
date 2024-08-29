b7b46fbe72e4efa70958aa847e1cf3702ba688e7
Raj Yadav <rajyadav751811@gmail.com>
2024-08-29 08:22:30 +0530

    Decoder Technique Implemented

--- 06.EncodingAndDecoding/src/Encoding.java
+++ 06.EncodingAndDecoding/src/Encoding.java
@@ -1,5 +1,6 @@
 import java.util.Base64;
 import java.util.Base64.Encoder;
+import java.util.Base64.Decoder;
 
 public class Encoding {
     public static void main(String[] args) {
@@ -9,5 +10,9 @@ public class Encoding {
         String encodedString = encoder.encodeToString(str.getBytes());
 
         System.out.println(encodedString);
+
+        Decoder decoder = Base64.getDecoder();
+        byte[] decode = decoder.decode(encodedString);
+        System.out.println(new String(decode));
     }
 }
